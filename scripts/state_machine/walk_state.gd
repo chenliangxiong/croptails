0747bc56a6f3db7dbc024313c6b449ecfad3dfc9
chenliangxiong <57304787+chenliangxiong@users.noreply.github.com>
2025-03-25 19:30:28 +0800

    build:创建项目，并且完成地图、角色移动的开发

---
+++ scripts/state_machine/walk_state.gd
@@ -0,0 +1,44 @@
+extends StateNode
+const gameInputEvents = preload("res://scripts/game_input_events.gd")
+
+@export var player: CharacterBody2D
+@export var animated_sprite_2d: AnimatedSprite2D
+@export var speed: int = 50
+
+var direction: Vector2
+
+func _on_process(_delta: float) -> void:
+	pass
+
+
+func _on_physics_process(_delta: float) -> void:
+	direction = GameInputEvents.movement_input()
+	if direction == Vector2.UP:
+		animated_sprite_2d.play("walk_back")
+	elif direction == Vector2.DOWN:
+		animated_sprite_2d.play("walk_front")
+	elif direction == Vector2.LEFT:
+		animated_sprite_2d.play("walk_left")
+	elif direction == Vector2.RIGHT:
+		animated_sprite_2d.play("walk_right")
+	else:
+		animated_sprite_2d.play("walk_front")
+
+	player.velocity = direction * speed
+	player.move_and_slide()
+
+
+func _on_unhandled_input(_event: InputEvent) -> void:
+	pass
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
