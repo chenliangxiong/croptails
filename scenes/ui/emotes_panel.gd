a1ad817551e5040e5335936dd90e4556b2cfb7e2
chenliangxiong <57304787+chenliangxiong@users.noreply.github.com>
2025-04-06 05:19:47 +0800

    feat:添加拾取物品时的表情

--- scenes/ui/emotes_panel.gd
+++ scenes/ui/emotes_panel.gd
@@ -8,6 +8,9 @@ var idle_emotes: Array = ["emote_1_idle", "emote_2_smile", "emote_3_ear_wave", "
 func _ready():
     animated_sprite_2d.play(idle_emotes[0])
 
+    InventoryManager.inventory_changed.connect(on_inventory_changed)
+    GameDialogueManager.feed_the_animals.connect(on_feed_the_animals)
+
 func play_emote(animation: String):
     animated_sprite_2d.play(animation)
 
@@ -16,3 +19,9 @@ func _on_emote_idle_timer_timeout():
     var emote = idle_emotes[index]
 
     animated_sprite_2d.play(emote)
+
+func on_inventory_changed():
+    animated_sprite_2d.play("emote_7_excited")
+
+func on_feed_the_animals():
+    animated_sprite_2d.play("emote_6_love_kiss")
